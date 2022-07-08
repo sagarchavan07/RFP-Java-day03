078e2ddb2df63a2e3159f66448e024ddb47b7aab
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-08 09:16:54 +0530

    Added : welcome message

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
 }
