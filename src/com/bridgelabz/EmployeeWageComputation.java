8c6adf67a4d3ccb4fcc8802fc393d61d5b2a7f3e
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-08 09:34:28 +0530

    Added : daily wage

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,15 +1,21 @@
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
+    static int WAGE_PER_HOUR=20;
+    static int FULL_DAY_HOUR=10;
     public static void main(String[] args) {
         int attendance;
+        int dailyWage=0;
 
         System.out.println("Welcome to Employee Wage Computation");
         attendance=checkAttendance();
         if (attendance == 1) {
             System.out.println("Employee is Present");
+            dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
         }else
             System.out.println("Employee is Absent");
+
+        System.out.println("Daily Wage = "+dailyWage);
     }
     static int checkAttendance(){
         int random=(int)(Math.random()*10)%2;
