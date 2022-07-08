f605fb237d4c17c2f1ba54f456c01a585fd3bff4
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-08 13:50:27 +0530

    Added : condition of total working hours or days

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -7,38 +7,42 @@ public class EmployeeWageComputation {
     static final int WAGE_PER_HOUR=20;
     static final int FULL_DAY_HOUR=10;
     static final int PART_TIME_HOUR=5;
+    static final int MAX_HOURS=100;
+    static final int MAX_DAYS=20;
     public static void main(String[] args) {
         int attendance;
         int dailyWage=0;
         int day=1;
         int totalWage=0;
+        int totalWorkingHrs=0;
 
         System.out.println("Welcome to Employee Wage Computation");
 
-        while (day <=20) {
-            System.out.println("Day #"+day);
+        while (day <=MAX_DAYS && totalWorkingHrs<MAX_HOURS) {
             attendance = checkAttendance();
 
             switch (attendance) {
                 case IS_FULL_TIME:
-                    System.out.println("Employee is full time");
                     dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    totalWage+=dailyWage;
+                    totalWorkingHrs+=FULL_DAY_HOUR;
                     break;
                 case IS_PART_TIME:
-                    System.out.println("Employee is part time");
                     dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    totalWage+=dailyWage;
+                    totalWorkingHrs+=PART_TIME_HOUR;
                     break;
                 default:
-                    System.out.println("Employee is Absent");
                     break;
             }
 
-            System.out.println("Daily Wage = " + dailyWage);
+            totalWage+=dailyWage;
+            System.out.println("Daily Wage day #"+day+" = "+ dailyWage);
             day++;
         }
+        System.out.println();
+        System.out.println("Total Days = "+(day-1));
+        System.out.println("Total Working Hours = "+totalWorkingHrs);
         System.out.println("Total Wage of month= "+totalWage);
+
     }
     static int checkAttendance(){
         int random=(int)(Math.random()*10)%3;
