f1261bfd759185ecb875f17b891358b1ee91b5d7
sagar <chavansagar96044@gmail.com>
2022-07-18 20:03:45 +0530

    manage employee wage of multiple companies using hashmap

--- src/com/bridgelabz/CompanyEmpWage.java
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -1,53 +1,24 @@
 package com.bridgelabz;
 
 public class CompanyEmpWage {
-    public enum Attendance{
-        IS_FULL_TIME,IS_PART_TIME
-    }
+    String companyName;
     static final int FULL_DAY_HOUR=10;
     static final int PART_TIME_HOUR=5;
-    int totalWage=0;
-    int totalWorkingHrs=0;
     int wagePerHour;
     int maxWorkingDays;
     int maxWorkingHours;
-    int empWageBuilder(){
-        Attendance attendance;
-        int dailyWage=0;
-        int day=1;
+    int totalWage;
+    int totalWorkingHrs;
 
-
-        while (day <= maxWorkingDays && totalWorkingHrs< maxWorkingHours) {
-            attendance = checkAttendance();
-
-            switch (attendance) {
-                case IS_FULL_TIME:
-                    if (totalWorkingHrs+FULL_DAY_HOUR <= maxWorkingHours) {
-                        dailyWage = wagePerHour * FULL_DAY_HOUR;
-                        totalWorkingHrs+=FULL_DAY_HOUR;
-                    }else {
-                        dailyWage = wagePerHour * PART_TIME_HOUR;
-                        totalWorkingHrs+=PART_TIME_HOUR;
-                    }
-                    break;
-                case IS_PART_TIME:
-                    dailyWage = wagePerHour * PART_TIME_HOUR;
-                    totalWorkingHrs+=PART_TIME_HOUR;
-                    break;
-                default:
-                    break;
-            }
-            totalWage+=dailyWage;
-//            System.out.println("day "+day+" wage= "+ dailyWage);
-            day++;
-        }
-        System.out.println();
-        System.out.println("Total Days = "+(day-1));
-        System.out.println("Total Working Hours = "+totalWorkingHrs);
-        return totalWage;
-    }
-    Attendance checkAttendance(){
-        int random=(int)(Math.random()*10)%3;
-        return (random== Attendance.IS_FULL_TIME.ordinal()) ? Attendance.IS_FULL_TIME: Attendance.IS_PART_TIME;
+    @Override
+    public String toString() {
+        return "CompanyEmpWage{" +
+                "companyName='" + companyName + '\'' +
+                ", wagePerHour=" + wagePerHour +
+                ", maxWorkingDays=" + maxWorkingDays +
+                ", maxWorkingHours=" + maxWorkingHours +
+                ", totalWage=" + totalWage +
+                ", totalWorkingHrs=" + totalWorkingHrs +
+                '}';
     }
 }
--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -6,22 +6,30 @@ public class EmployeeWageComputation {
         System.out.println("Welcome to Employee Wage Computation");
         System.out.println("Employee Wage Computation for company1");
         CompanyEmpWage company1=new CompanyEmpWage();
+        company1.companyName="company1";
         company1.wagePerHour=20;
         company1.maxWorkingDays=20;
         company1.maxWorkingHours=150;
-        company1.empWageBuilder();
+
+        EmpWageBuilder empWageBuilder=new EmpWageBuilder();
+        empWageBuilder.wageBuilder(company1);
         System.out.println("Total Wage of company1= "+company1.totalWage);
 
         System.out.println("*********************************************\n"
                 +"Employee Wage Computation for company2");
         CompanyEmpWage company2=new CompanyEmpWage();
+        company2.companyName="company2";
         company2.wagePerHour=25;
         company2.maxWorkingDays=23;
         company2.maxWorkingHours=160;
-        company2.empWageBuilder();
+
+        empWageBuilder.wageBuilder(company2);
         System.out.println("Total Wage of company1= "+company2.totalWage);
+
+        System.out.println("***********************************");
+        for (String key: empWageBuilder.CompanyEmpWageList.keySet()) {
+            System.out.println(empWageBuilder.CompanyEmpWageList.get(key).toString());;
+        }
     }
-
-
 
 }
